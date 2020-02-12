c75c27182bb03039b593850f5fec9168104c5c5a
Metlover <edwards1860@gmail.com>
2020-02-11 19:14:24 -0800

    Updated with new hero_vals

--- Scripts/clean_hero_vals.R
+++ Scripts/clean_hero_vals.R
@@ -1,4 +1,5 @@
 setwd("~/OWL/stats")
+require(owlstats)
 temp = list.files(pattern="*.csv")
 myfiles = lapply(temp, readr::read_csv)
 hero_vals = data.table::rbindlist(myfiles)
@@ -10,8 +11,8 @@ slugify <- function(x, alphanum_replace="", space_replace="_", tolower=TRUE) {
   return(x)
 }
 hero_vals$stat_name = slugify(hero_vals$stat_name)
-players = owlstats::getPlayers()
-teams = owlstats::getTeams()
+players = getPlayers()
+teams = getTeams()
 hero_vals = reshape2::dcast(hero_vals, start_time + match_id + stage + map_type + map_name + player + team + hero ~ stat_name, value.var = 'stat_amount', fun.aggregate = sum)
 hero_vals_temp = merge(hero_vals, players[,c('player_name','player_id')], by.x=c('player'),by.y=c('player_name'),all.x=T)
 hero_vals_temp = merge(hero_vals_temp, teams[,c('team_name','team_id')], by.x=c('team'),by.y=c('team_name'),all.x=T)
